ca98552c5810df925fae7e7136ad331dcd5f3e39
mahi <mahima2071987@gmail.com>
2023-10-10 11:09:18 +0530

    Delete EC@2 directory

--- EC@2/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo cd /var/www/html
-sudo mkdir web
-sudo cd web
-sudo touch index.html
-echo "Deploy a web server on aws" | sudo tee /var/www/html/web/index.html
